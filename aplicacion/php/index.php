6b08a44b26a2ea1ef1d27115077f871d17059ded
exilonm <alejandroramirezjimenez@gmail.com>
2019-10-01 23:10:23 +0200

    nuevo php

--- aplicacion/php/index.php
+++ aplicacion/php/index.php
@@ -1,8 +1,19 @@
-<?php 
+<?php
 
-$tempTitle = 'https://hasquiz.com/ver/quizz/Cual-seria-tu-X-Men-superpoder-o-mutacion?n=30';
-echo $tempTitle;
+$tempTitle = 'https://hasquiz.com/ver/quizz/Cual-seria-tu-X-Men-superpoder-o-mutacion?n=39';
+$id = explode("?n=", $tempTitle)[1];
 
+$conexion = new PDO('mysql:host=localhost;dbname=hasquiz;charset=utf8', 'root', '');
+
+
+
+
+$stm = $conexion->prepare('SELECT titulo FROM quizz WHERE id = ?');
+$stm->bindValue(1, $id);
+$stm->execute();
+
+$row = $stm->fetch(PDO::FETCH_ASSOC);
+$tituloQuiz = $row['titulo'];
 ?>
 
 
@@ -13,9 +24,10 @@ echo $tempTitle;
 <meta name='twitter:card' content='summary_large_image'>
 <meta name='twitter:site' content='@hasquiz'>
 <meta name='twitter:creator' content='@hasquiz'>
-<meta name='twitter:title' content='Probemos esto loco'>
-<meta name='twitter:description' content='PHP nos salvará.'>
-<meta name='twitter:image' content='http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg'>
+<meta name='twitter:title' content='<?php echo $tituloQuiz;?>'>
+<meta name='twitter:description' content='La nueva página web de quizzes, ¡Crea los tuyos propios!.'>
+<meta name='twitter:image' content='https://firebasestorage.googleapis.com/v0/b/buzzflix-c94e0.appspot.com/o/HASQUIZ_Mesa%20de%20trabajo%201.png?alt=media&token=9bc9ae58-b709-4f5a-985d-1d0185ee88e2'>
+
 <head>
 
 </head>
